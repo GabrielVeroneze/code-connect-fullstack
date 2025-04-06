f02e7184841bb2ca7a887bd14ffcd00801effed7
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-04-06 17:43:12 -0300

    ♻️ refactor: simplifica estrutura da interface PaginatedPosts

--- src/types/PaginatedPosts.ts
+++ src/types/PaginatedPosts.ts
@@ -1,11 +1,7 @@
 import { Post } from '@/types/Post'
 
 export interface PaginatedPosts {
-    first: number
+    data: Post[]
     prev: number | null
     next: number | null
-    last: number
-    pages: number
-    items: number
-    data: Post[]
 }
